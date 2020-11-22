f9a28ac00c868a16e554b3c8c787215b1d5aa1e8
Marlena Janasz <mar.janasz87@gmail.com>
2020-11-22 17:37:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,81 @@
       
 // 1 sprawdzenie czy jest samymi cyframi  -> ale type number w html to sprawdza???
-      function onlyNumbers(number) {
-          return /^\d+$/.test(number);
-      };
+  function onlyNumbers(number) {
+    return /^\d+$/.test(number);
+  };
 
-      // 2 czy jest odpowiedniej długości
-      function canBeNumber(number) {
-        if (onlyNumbers(number)) {
-          return [13, 15, 16].includes(number.length)
+        // 2 czy jest odpowiedniej długości
+        function canBeNumber(number) {
+          if (onlyNumbers(number)) {
+            return [13, 15, 16].includes(number.length)
+          }
+          return false
         }
-        return false
-      }
 
-      // 3 czy to visa
-      function canBeVisa(number) {
-        if(number.length === 15){
-          return false;
-        } else if (number[0] === '4'){
-          return true;
+        // 3 czy to visa
+        function canBeVisa(number) {
+          if(number.length === 15){
+            return false;
+          } else if (number[0] === '4'){
+            return true;
+          } else {
+            return false;
+          }
         }
-      }
-      //4 czy master card
-      function canBeMaster(number) {
-        if(number.length === 15 || number.length === 13){
-          return false;
-        }  else if ('51'<=number.substring(0,2)<='55' || '2221'<=number.substring(0,4)<='2720'){
-          return true;
+        //4 czy master card
+        function canBeMaster(number) {
+          if(number.length === 15 || number.length === 13){
+            return false;
+          }  else if ('51'<=number.substring(0,2)<='55' || '2221'<=number.substring(0,4)<='2720'){
+            return true;
+          } else {
+            return false;
+          }
         }
-      }
-      //5 czy  to american
-      function canBeAmerican(number) {
-        if(number.length === 16 || number.length === 13){
-          return false;
-        } else if (number.substring(0,2)==='34' || number.substring(0,2)==='37'){
-          return true;
+        //5 czy  to american
+        function canBeAmerican(number) {
+          if(number.length === 16 || number.length === 13){
+            return false;
+          } else if (number.substring(0,2)==='34' || number.substring(0,2)==='37'){
+            return true;
+          } else {
+            return false;
+          }
         }
-      }
-      
+        
 
-      // ta wywołuje się na start 
-      function checkCardNumber(number) {
+        // ta wywołuje się na start 
+        function checkCardNumber(number) {
+          const cardType = document.getElementById("cardType");
           if (canBeNumber(number)) {
             if (canBeVisa(number)) {
-              document.getElementById("cardType").classList.add("visa");
-            //console.log('To karta Visa')
+            cardType.classList.add("visa");
             }
             else if (canBeMaster(number)) {
-              document.getElementById("cardType").classList.add("master");
-            //console.log('Master card')
+              cardType.classList.add("master");
             }
             else if (canBeAmerican(number)) {
-            document.getElementById("cardType").classList.add("amex");
-            //console.log('American Express')
+              cardType.classList.add("amex");
             }
             else {
-            console.log('Karty nie rozpoznano')
-            document.getElementById("cardType").classList.add("other");
+              cardType.classList.add("other");
             }
           }
           else {
             alert("Niewłaściwa długość karty!")
           }
-      }
-
-      function inputValue(){
-        if(document.querySelector("input").value !== ""){
-          checkCardNumber(document.querySelector("input").value)
         }
-        document.querySelector("input").value = "";
-      }
 
-      document.querySelector("button").onclick = inputValue;
+        function inputValue(){
+          const input = document.querySelector("input");
+          if(input.value !== ""){
+            checkCardNumber(input.value)
+          }
+          input.value = "";
+        }
+
+        document.querySelector("button").onclick = inputValue;
+
+
 
    
